8baf2524aa743f1b9af004bd81c9c5aae5e624cc
MaciejBros <115492219+MaciejBros@users.noreply.github.com>
2025-04-09 18:05:02 +0200

    feat: add models

---
+++ Przychodnia/Models/Pacjent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Przychodnia.Models
+{
+    public class Pacjent : Osoba
+    {
+        [Required]
+        [StringLength(30)]
+        public string Login { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Haslo { get; set; }
+
+        public ICollection<Wizyta> Wizyty { get; set; }
+    }
+}
