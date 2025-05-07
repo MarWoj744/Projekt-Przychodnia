7d14b48289cd278ef82de4e55b7a5e2fa003fb33
JakubCzul <jc305354@student.polsl.pl>
2025-05-07 09:36:11 +0200

    Tests

---
+++ BLLTests/MockTest.cs
@@ -0,0 +1,43 @@
+﻿using DTOs;
+using IBLL;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BLLTests
+{
+    public class MockTest
+    {
+        [Fact]
+        public async Task Mock_VerifyMethodWasCalled()
+        {
+            var mock = new Mock<IWizytaService>();
+            var dto = new RejestracjaWizytyDTO
+            {
+                PacjentId = 1,
+                DataWizyty = DateTime.Now.AddDays(1),
+                LekarzId = 101,
+                Opis = "Konsultacja"
+            };
+
+            // Sprawdzamy, czy mock poprawnie ustawi metodę ZarejestrujWizyteAsync
+            mock.Setup(m => m.ZarejestrujWizyteAsync(It.IsAny<RejestracjaWizytyDTO>()))
+                .ReturnsAsync(true);
+
+            var result = await mock.Object.ZarejestrujWizyteAsync(dto);
+
+            Assert.True(result);
+            Assert.Equal(1, dto.PacjentId);
+            Assert.Equal("Konsultacja", dto.Opis);
+            mock.Verify(m => m.ZarejestrujWizyteAsync(dto), Times.Once);
+
+            var anotherResult = await mock.Object.ZarejestrujWizyteAsync(new RejestracjaWizytyDTO());
+            Assert.True(anotherResult);
+            mock.Verify(m => m.ZarejestrujWizyteAsync(It.IsAny<RejestracjaWizytyDTO>()), Times.Exactly(2));
+        }
+    }
+}
