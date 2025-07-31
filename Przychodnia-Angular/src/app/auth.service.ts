abf378b6343edce48cc16de3e2049e10cfff5258
maciejbros <maciej.bros02@gmail.com>
2025-07-31 17:09:46 +0200

    angular init

---
+++ Przychodnia-Angular/src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+export interface RegisterDTO {
+  imie: string;
+  nazwisko: string;
+  pesel: string;
+  adres: string;
+  telefon: string;
+  email: string;
+  login: string;
+  haslo: string;
+  potwierdzHaslo: string;
+  rola: string; 
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:5120/api/auth'; 
+
+  constructor(private http: HttpClient) {}
+
+  register(userData: RegisterDTO): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+}
